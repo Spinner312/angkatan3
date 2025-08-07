91a475700888aae740aa05c731fa3ef3bc0e2785
CNDGA <cindagaargya@gmail.com>
2025-08-07 10:03:01 +0700

    first commit

--- belajar php/PHP/php-2.php
+++ belajar php/PHP/php-2.php
@@ -1,1 +1,21 @@
 <?php
+// perkondisian / percabangan
+// if else 
+// if else if else
+// switch
+$n = 10;
+if ($n < 20){
+  echo "betullllll";
+} else{
+  echo "salahhh" . "<br>";
+}
+$x = 30;
+if ($x < 20){
+    echo "benarrr";
+}else if ($x == 20){
+  echo "ini resultnya";
+} else {
+  echo "salah";
+}
+
+ 
